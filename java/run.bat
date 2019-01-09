d04093369b5423304e5c2d9bf5fccd265fc2bf4e
dpjames <outfieldslayer@DESKTOP-7QMJS7D.localdomain>
2019-01-09 00:55:22 -0800

    hopefully the final fix for big files. uploading the zip to drive now....

--- java/run.bat
+++ java/run.bat
@@ -1,1 +1,1 @@
-javac *.java && java Controller 2000 1500 ../data/bigdem.json ../data/biglandcover.json
+javac *.java && java Controller 2000 1500 ../../data/bigDEM.json ../../data/bigLandcover.json
